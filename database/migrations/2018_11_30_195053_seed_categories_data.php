8f231058413dfbc3a7efd24d3f6c5d025a3c4c41
lvyinhong <yinhong.lv@gmail.com>
2018-11-30 11:52:22 +0000

    Create categories

--- database/migrations/2018_11_30_195053_seed_categories_data.php
+++ database/migrations/2018_11_30_195053_seed_categories_data.php
@@ -15,21 +15,21 @@ class SeedCategoriesData extends Migration
     {
         $categories = [
             [
-                'name' => '分享',
-                'description' => '分享创造，分享发现'
+                'name'        => '分享',
+                'description' => '分享创造，分享发现',
             ],
             [
-                'name' => '教程',
-                'description' => '开发技巧,腿脚扩展包等',
+                'name'        => '教程',
+                'description' => '开发技巧、推荐扩展包等',
             ],
             [
-                'name' => '问答',
-                'desctiption' => '请保持友善，互帮互助',
+                'name'        => '问答',
+                'description' => '请保持友善，互帮互助',
             ],
             [
-                'name' => '公告',
-                'description' => '站点公告'
-            ]
+                'name'        => '公告',
+                'description' => '站点公告',
+            ],
         ];
 
         DB::table('categories')->insert($categories);

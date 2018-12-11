d209b9764fe597e539b0169e2b127f812e92f174
Shawn_davi <yinhong.lv@foxmail.com>
2018-12-11 12:13:58 +0000

    Integrate laravel-permission package

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,9 +5,12 @@ namespace App\Models;
 use Auth;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
+    use HasRoles;
+
     public function markAsRead()
     {
         $this->notification_count = 0;

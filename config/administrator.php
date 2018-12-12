9ed62fb7bb4190b9db67d3f5d0d51fd0aeabd2bc
lvxiaohong <yinhong.lv@foxmail.com>
2018-12-12 11:45:43 +0800

    Admin can manage users

--- config/administrator.php
+++ config/administrator.php
@@ -72,7 +72,7 @@ return array(
      */
     'permission' => function () {
         // 只要是能管理内容的用户，就允许访问后台
-        return Auth::check() && Auth()->can('manage_contents');
+        return Auth::check() && Auth::user()->can('manage_contents');
     },
 
     /*
